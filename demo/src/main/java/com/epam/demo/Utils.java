504ab9a0a0e68c9842bd0f2b7cb983e2fea01128
Vahe <vahe.jaloyan34@gmail.com>
2023-11-02 21:03:09 +0400

    try 2

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,11 +1,17 @@
 package com.epam.demo;
+
+import com.epam.utils.StringUtils;
 import java.util.List;
 
 
 public class Utils {
+
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
-        return args.stream()
-                .allMatch(StringUtils::isPositiveNumber);
+        for (String n : args) {
+            if (!StringUtils.isPositiveNumber(n)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
+
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
+        if (NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
